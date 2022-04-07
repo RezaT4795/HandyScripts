7ffb85b3d516d6017ccd14bf0e6ab245a949f27f
Reza Talebi <reza.talebi.73@outlook.com>
2022-04-08 04:28:35 +0430

    Added some detailed comments for clarification.
    Signed-off-by: Reza Talebi <reza.talebi.73@outlook.com>

--- Python/patch_splitter.py
+++ Python/patch_splitter.py
@@ -1,21 +1,22 @@
 from os import system
 
-# Getting the content of the patch and splitting it
+# Getting the content of the patch and splitting it.
 diff_file = open('patch.diff', 'r')
 diff_content = diff_file.read()
 diff_splits = diff_content.split('diff --git')
-diff_splits.remove(diff_splits[0])
+diff_splits.remove(diff_splits[0]) # Removes the first element which is empty.
 for i in range(len(diff_splits)):
-    diff_splits[i] = "diff --git"+diff_splits[i]
+    diff_splits[i] = "diff --git"+diff_splits[i] # Adds the missing "diff --git" at the beginning of all patch splits.
 
-# Creating empty files and filling them with each split
+# Creating empty files and filling them with each patch split.
 create_file = "touch "
-
 for i in range(len(diff_splits)):
-    file_name = str(i+1).zfill(len(str(len(diff_splits))))+".diff"
-    print("Splitting the patch [%s/%i]" % (file_name.removesuffix('.diff'), len(diff_splits)))
+    # Changes file extentions to ".diff"
+    # And name each file with a number, depending on the digits lenght of total patch splits.
+    file_name = str(i+1).zfill(len(str(len(diff_splits))))+".diff" # Example: 01.diff, 02.diff,..., 99.diff 
+    print("Splitting the patch [%s/%i]" % (file_name.removesuffix('.diff'), len(diff_splits))) # A simple progress indicator.
     empty_file = system(create_file+file_name)
-    # Writing content to each empty file after creation
+    # Writing patch splits to each empty file after creation.
     result = open(file_name, 'w')
     result.write(diff_splits[i])
     result.close
