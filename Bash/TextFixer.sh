ab83b600267c59f469f2a5b7488865598463ea96
Reza Talebi <reza.talebi.73@outlook.com>
2022-04-24 20:24:47 +0430

    Bash/TextFixer.sh: Minor improvements.
    Signed-off-by: Reza Talebi <reza.talebi.73@outlook.com>

--- Bash/TextFixer.sh
+++ Bash/TextFixer.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+# Create a temporary variable
+newfile="test"
 # Iterate through all files and directories inside the working directory. 
 for file in $(find $(pwd)/* -name '*');
 do
         FILE_NAME=$(echo $file) # Saving filename.
         if [[ ! $FILE_NAME == $(pwd)"/script.sh" ]]; then # Excluding the script itself.
                 echo "Processing file:"
-                sed -i -e 's/wrong//g' $file # Remove all occurances of the string (For example: "wrong" word).
                 echo $file
+                sed -e "s/\r//g" $file > $newfile # Removes all occurances of the given string or statement (eg: CONTROL-M or ^M) and save it to a temporary file.
+                rm $file # Removes the original file.
+                mv $newfile $file # Renaming the new file to original name of the file.
         fi
 done 
